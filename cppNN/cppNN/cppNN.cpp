e91bd696abc03549614f4eaf094b4779fd1b1daa
Nobuhide Tsuda <ntsuda@master.email.ne.jp>
2024-12-19 07:49:25 +0900

    0003 ◎ C 12/19 12/19 0.001│ToDo│Layer クラス追加

--- cppNN/cppNN/cppNN.cpp
+++ cppNN/cppNN/cppNN.cpp
@@ -4,6 +4,6 @@
 Network::Network(int nInput)
 	: m_nInput(nInput)
 {
-	m_vinput.resize(nInput+1);		//	+1 for バイアス
-	m_vinput[0] = 1.0;
+	m_input.resize(nInput+1);		//	+1 for バイアス
+	m_input[0] = 1.0;
 }
--- cppNN/cppNN/cppNN.h
+++ cppNN/cppNN/cppNN.h
@@ -1,12 +1,20 @@
 #pragma once
 
 #include <vector>
+#include <memory>
 
-
-class Network {
+class Network {		//	シーケンシャルネットワーククラス
 public:
 	Network(int nInput);		//	入力次元
 private:
 	int		m_nInput;
-	std::vector<float>	m_vinput;		//	入力値
+	std::vector<float>	m_input;						//	入力値
+	std::vector<std::auto_ptr<class Layer>>	m_vlayers;		//	各レイヤーへのオートポインタ配列
 };
+
+class Layer {		//	シーケンシャルネットワーク各レイヤー基底クラス
+public:
+	Layer() {}
+	virtual ~Layer() = 0;
+};
+
